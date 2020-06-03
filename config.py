6943f520379c149bdb86375a33beef419707ac2c
Aditya Jajodia <aadityajajodia@gmail.com>
2020-06-03 14:06:21 -0400

    changes for deployment

--- config.py
+++ config.py
@@ -4,4 +4,8 @@ class Config(object):
     SECRET_KEY =os.environ.get('SECRET_KEY') or "secret_string"
 
 
-    MONGODB_SETTINGS ={'db':'cgsuppliers'}
+
+    MONGODB_SETTINGS ={
+        'db':'cgsuppliers',
+        'host':"mongodb+srv://admin:admin@cluster0-b8t27.mongodb.net/test?retryWrites=true&w=majority"
+        }
