e6adf8f72ea1d4bf59d2ba2b3b1e1afe282822fd
Aditya Jajodia <aadityajajodia@gmail.com>
2020-06-03 12:58:57 -0400

    Added Admin User

--- application/forms.py
+++ application/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, SelectField,RadioField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, IntegerField, SelectField,RadioField,FileField
 from wtforms.validators import DataRequired, Email, ValidationError, URL
 from application.models import Property
 
@@ -8,38 +8,23 @@ import re
 
 # (value, string shown in the dropdown)
 choices = {
-    "foodPreference" :[
-        ('VEGAN','VEGAN'),
-        ('VEG','VEG'),
-        ('NON VEG','NON VEG'),
-        ('ANY','ANY'),
-    ],
-    "genderPreference":[
-        ('MALE','MALE'),
-        ('FEMALE','FEMALE'),
-        ('BOTH','BOTH'),
-    ],
-    "bedsAvailable" : [
-        ('1','1'),
-        ('2','2'),
-        ('3','3'),
-        ('4','4'),
-        ('5','5'),
-    ],
-    "roomateCount":[
-        ('0','0'),
-        ('1','1'),
-        ('2','2'),
-        ('3','3'),
-        ('4','4'),
-        ('5','5'),
-    ],
-    "resourceType":[
-        ('Medicine','Medicine'),
-        ('Food','Food')
+    "productTypes" : [
+        ('Award','Award'),
+        ('Lapel Pin','Lapel Pin'),
+        ('Personalized Gifts','Personalized Gifts'),
+        ('Printing','Printing')
     ]
 
 }
+
+class insertProdcutForm(FlaskForm):
+    productName         =   StringField("Product Name",validators=[DataRequired()])
+    category            =   SelectField("Selecte Product Category", choices=choices["productTypes"],validators=[DataRequired()])
+    price               =   IntegerField("Price",validators=[DataRequired()])
+    imageFile           =   FileField("Image",validators=[DataRequired()])
+    submit              =   SubmitField("Add to Database",validators=[DataRequired()])
+
+
 class adminLoginForm(FlaskForm):
     email               =   StringField("Email",validators=[DataRequired(),Email()])
     password            =   PasswordField("Password",validators=[DataRequired()])
@@ -51,79 +36,3 @@ class contactUsQueryForm(FlaskForm):
     queryDescription    =   StringField("Query Description",validators=[DataRequired()])
     submit              =   SubmitField("Submit",validators=[DataRequired()])
 
-class AddPropertyForm(FlaskForm):
-    # dt = DateField('DatePicker', format='%Y-%m-%d')
-
-
-
-    email               =   StringField("Email",validators=[DataRequired(),Email()])
-    mapsLocation        =   StringField("Google Maps Location",validators=[DataRequired()])
-    startDate           =   DateField("Start Date",validators=[DataRequired()])
-    endDate             =   DateField("End Date",validators=[DataRequired()])
-    foodPreference      =   RadioField("Food Preference",choices=choices["foodPreference"],validators=[DataRequired()])
-    genderPreference    =   RadioField("Gender Preference",choices=choices["genderPreference"],validators=[DataRequired()])
-    bedsAvailable       =   SelectField("Beds Available", choices=choices["bedsAvailable"],validators=[DataRequired()])
-    roommateCount       =   SelectField("Number of Roommates", choices=choices["roomateCount"],validators=[DataRequired()])
-    additionalFeatures  =   StringField("Additional Features",validators=[DataRequired()])
-    pricePerBed         =   IntegerField("Price/Bed",validators=[DataRequired()])
-    submit              =   SubmitField("Submit",validators=[DataRequired()])
-
-    def validate_email(self,email):
-        pEmail=Property.objects(email=email.data).first()
-        if pEmail:
-            raise ValidationError("Email is already in use. Pick another one")
-
-
-    def validate_endDate(self, endDate):
-        # print(self.startDate.data, endDate.data)
-        if self.startDate.data is not None and endDate.data is not None and self.startDate.data >= endDate.data:
-            raise ValidationError("End date cannot be before or same as start date")
-
-    def validate_mapsLocation(self,mapsLocation):
-        # mapsRegex = "/^https?\:\/\/(www\.|maps\.)?google\.[a-z]+\/maps\/?\?([^&]+&)*(ll=-?[0-9]{1,2}\.[0-9]+,-?[0-9]{1,2}\.[0-9]+|q=[^&]+)+($|&)/";
-        # mapsRegex = re.compile(r'/^https?\:\/\/(www\.|maps\.)?google\.[a-z]+\/maps\/?\?([^&]+&)*(ll=-?[0-9]{1,2}\.[0-9]+,-?[0-9]{1,2}\.[0-9]+|q=[^&]+)+($|&)/') 
-        # mapsRegex=re.compile(r'^(http(s?)://)?maps\.google(\.|/).*/maps/.*$')
-        # mapsRegex1=re.compile(r'/google.com\/maps/')
-        # if not mapsRegex1.match(str(mapsLocation)):
-        #     raise ValidationError("Not a google maps location")
-        check1="google.com/maps"
-        check2="maps.google"
-        if check1 not in str(mapsLocation) and check2 not in str(mapsLocation):
-            raise ValidationError("Not a google maps location")
-
-    def validate_pricePerBed(self,pricePerBed):
-        if pricePerBed.data>20000 or pricePerBed.data<1:
-            raise ValidationError("Enter valid amount")
-
-class CovidResourcesForm(FlaskForm):
-    email                       =   StringField("Email",validators=[DataRequired(),Email()])
-    resourceType                =   SelectField("Selecte Resource Type", choices=choices["resourceType"],validators=[DataRequired()])
-    dateAvailableFrom           =   DateField("Date Available From",validators=[DataRequired()])
-    submit                      =   SubmitField("Submit",validators=[DataRequired()])
-
-
-class CovidLoginForm(FlaskForm):
-    email               =   StringField("Email",validators=[DataRequired(),Email()])
-    name                =   StringField("Name",validators=[DataRequired()])
-    age                 =   IntegerField("Age",validators=[DataRequired()])
-    languageSpoken                 =   IntegerField("Language Spoken",validators=[DataRequired()])
-    locationZipCode     =   IntegerField("Zip Code of your Work Location",validators=[DataRequired()])
-    idNumber            =   IntegerField("ID Number",validators=[DataRequired()])
-    submit              =   SubmitField("Submit",validators=[DataRequired()])
-
-class AddHotelPropertyForm(FlaskForm):
-    # dt = DateField('DatePicker', format='%Y-%m-%d')
-
-
-
-    email               =   StringField("Email",validators=[DataRequired(),Email()])
-    mapsLocation        =   StringField("Google Maps Location",validators=[DataRequired()])
-    # startDate           =   DateField("Start Date",validators=[DataRequired()])
-    # endDate             =   DateField("End Date",validators=[DataRequired()])
-    # foodPreference      =   RadioField("Food Preference",choices=choices["foodPreference"],validators=[DataRequired()])
-    # genderPreference    =   RadioField("Gender Preference",choices=choices["genderPreference"],validators=[DataRequired()])
-    roomsAvailable       =   IntegerField("Rooms Available",validators=[DataRequired()])
-    # roommateCount       =   SelectField("Number of Roommates", choices=choices["roomateCount"],validators=[DataRequired()])
-    additionalFeatures  =   StringField("Additional Features",validators=[DataRequired()])
-    pricePerRoom         =   IntegerField("Price/Bed",validators=[DataRequired()])
-    submit              =   SubmitField("Submit",validators=[DataRequired()])
--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,13 @@
 from application import app, db
-from flask import render_template, redirect, flash, url_for
-from application.models import Property, covidResources, covidLogin,HotelProperty,contactUsQuery,adminLogin
-from application.forms import AddPropertyForm, CovidResourcesForm, CovidLoginForm,AddHotelPropertyForm,contactUsQueryForm,adminLoginForm
+from flask import render_template, redirect, flash, url_for,request
+# from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from application.models import contactUsQuery,adminLogin,insertProduct
+from application.forms import contactUsQueryForm,adminLoginForm,insertProdcutForm
+import codecs
+# photos = UploadSet('photos', IMAGES)
+# configure_uploads(app, photos)
+# patch_request_class(app)  # set maximum file size, default is 16MB
+
 
 @app.route("/")
 def index():
@@ -11,7 +17,22 @@ def index():
 @app.route("/product/")
 @app.route("/product/<productType>")
 def productPage(productType='award'):
-    return render_template("product.html",productType=productType)
+    categoryMapping={}
+    categoryMapping['award']='Award'
+    categoryMapping['pg']='Personalized Gifts'
+    categoryMapping['lapelpin']='Lapel Pin'
+    categoryMapping['printing']='Printing'
+    
+    items=insertProduct.objects(category=categoryMapping[productType])
+    # for item in items:
+    #     print(item.productName)    
+        # base64_data = codecs.encode(item.imageFile.read(), 'base64')
+        # image = base64_data.decode('utf-8')
+        # t=codecs.encode(item.imageFile.read(), 'base64')
+        # t=t.decode('utf-8')
+        # return image
+    # return None
+    return render_template("product.html",productType=productType,items=items,codecs=codecs)
 
 @app.route("/contactus",methods=['GET','POST'])
 def contactus():
@@ -40,108 +61,15 @@ def adminLoginView():
     return render_template("adminLogin.html",form=form)
 
 
-@app.route("/admin")
+@app.route("/admin",methods=['GET','POST'])
 def admin():
-    
-    return render_template("admin.html")
-
-# # @app.route("/index")
-# @app.route("/sublease")
-# def index():
-#     return render_template("index.html", index=True )
-
-# @app.route("/login")
-# def login():
-#     return render_template("login.html", login=True )
-
-# @app.route("/addCovidResources",methods=['GET','POST'])
-# def addCovidResources():
-#     form= CovidResourcesForm()
-#     if form.validate_on_submit():
-#         email                       =   form.email.data
-#         resourceType                =   form.resourceType.data
-#         dateAvailableFrom           =   form.dateAvailableFrom.data
-#         covidResources(email=email,resourceType=resourceType,dateAvailableFrom=dateAvailableFrom).save()
-#         flash("You have successfully added resource. Thank you for your support")
-#         return redirect(url_for("index"))
-#     return render_template("addCovidResources.html", form=form, addCovidResources=True)
-
-# @app.route("/addCovidLogin",methods=['GET','POST'])
-# def addCovidLogin():
-#     form= CovidLoginForm()
-#     if form.validate_on_submit():
-#         email               =   form.email.data
-#         name                =   form.name.data
-#         age                 =   form.age.data
-#         locationZipCode     =   form.locationZipCode.data
-#         idNumber            =   form.idNumber.data
-#         covidLogin(email=email,name=name,age=age,locationZipCode=locationZipCode, idNumber=idNumber).save()
-#         flash("You have been successfully added to the database. We will find you help.")
-#         return redirect(url_for("index"))
-#     return render_template("addCovidLogin.html", form=form, addCovidLogin=True)
-
-# @app.route("/addHotel",methods=['GET','POST'])
-# def addHotel():
-#     form= AddHotelPropertyForm()
-#     if form.validate_on_submit():
-#         email               =   form.email.data
-#         mapsLocation                =   form.mapsLocation.data
-#         roomsAvailable                 =   form.roomsAvailable.data
-#         additionalFeatures     =   form.additionalFeatures.data
-#         pricePerRoom            =   form.pricePerRoom.data
-#         HotelProperty(email=email,mapsLocation=mapsLocation,roomsAvailable=roomsAvailable,additionalFeatures=additionalFeatures, pricePerRoom=pricePerRoom).save()
-#         flash("You have been successfully added to the database. We will find you help.")
-#         return redirect(url_for("covidHelp"))
-#     return render_template("addHotel.html", form=form, addHotel=True)
-
-# @app.route("/listHotel", methods=['GET'])
-# def listHotel():
-#     hotels = HotelProperty.objects.all()
-#     return render_template("listHotel.html", hotels=hotels, listHotel=True)
-
-
-# @app.route("/listProperty", methods=['GET'])
-# def listProperty():
-#     properties = Property.objects.all()
-#     return render_template("listProperty.html", properties=properties, listProperty=True)
-
-
-# @app.route("/listCovidResources", methods=['GET'])
-# def listCovidResources():
-#     covid_resources = covidResources.objects.all()
-#     print(covid_resources);
-#     return render_template("listCovidResources.html", covid_resources=covid_resources, listCovidResources=True)
-
-# @app.route("/addProperty",methods=['GET','POST'])
-# def addProperty():
-#     form= AddPropertyForm()
-#     if form.validate_on_submit():
-#         email               =   form.email.data
-#         mapsLocation        =   form.mapsLocation.data
-#         startDate           =   form.startDate.data
-#         endDate             =   form.endDate.data
-#         foodPreference      =   form.foodPreference.data
-#         genderPreference    =   form.genderPreference.data
-#         bedsAvailable       =   form.bedsAvailable.data
-#         roommateCount       =   form.roommateCount.data
-#         additionalFeatures  =   form.additionalFeatures.data
-#         pricePerBed         =   form.pricePerBed.data
-#         Property(email=email,mapsLocation=mapsLocation,startDate=startDate,
-#         endDate=endDate,foodPreference=foodPreference,
-#         genderPreference=genderPreference,bedsAvailable=int(bedsAvailable),
-#         roommateCount=int(roommateCount),additionalFeatures=additionalFeatures,
-#         pricePerBed=int(pricePerBed)).save()
-#         print("hello")
-#         flash("You have successfully added the property")
-#         return redirect(url_for("index"))
-#     return render_template("addProperty.html", form=form, addProperty=True)
-
-
-
-
-
-# @app.route("/addFakeLocation")
-# def addFakeLocation():
-#     Property(email="something@something.com",mapsLocation="myHouse",startDate="1/1/2001",endDate="2/2/2002",foodPreference="veg",
-#     genderPreference="male",bedsAvailable=2,roommateCount=2,additionalFeatures="laundary room",pricePerBed=600).save()
-#     return render_template("addFakeLocation.html")
+    form=insertProdcutForm()
+    if form.validate_on_submit():
+        productName               =   form.productName.data
+        category            =   form.category.data
+        price   =   form.price.data
+        imageFile   =   form.imageFile.data
+        insertProduct(productName=productName,category=category,price=int(price),imageFile=imageFile).save()
+        flash("Product Added Successfully",'success')
+        return redirect(url_for("admin"))
+    return render_template("admin.html",form=form)
